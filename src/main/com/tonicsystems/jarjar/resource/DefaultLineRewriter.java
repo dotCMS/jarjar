297c2dc19b337bbeba2b742fe29441a999ed0ca0
Jonathan Gamba <jonathan.gamba@dotcms.com>
2013-11-15 10:40:30 -0600

    Added list of allowed resources to modify.

--- src/main/com/tonicsystems/jarjar/resource/DefaultLineRewriter.java
+++ src/main/com/tonicsystems/jarjar/resource/DefaultLineRewriter.java
@@ -3,10 +3,13 @@ package com.tonicsystems.jarjar.resource;
 import com.tonicsystems.jarjar.Rule;
 import com.tonicsystems.jarjar.util.EntryStruct;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class DefaultLineRewriter implements LineRewriter {
+
+    private List<String> allowedFiles = new ArrayList<String>();
 
     private final List<MatchableRule> rules = new LinkedList<MatchableRule>();
 
@@ -14,11 +17,32 @@ public class DefaultLineRewriter implements LineRewriter {
         for (Rule rule : ruleList) {
             rules.add(new MatchableRule(rule));
         }
+
+        allowedFiles.add(".xml");
+        allowedFiles.add(".xsd");
+        allowedFiles.add(".tld");
+        allowedFiles.add(".properties");
+        allowedFiles.add(".conf");
+        allowedFiles.add(".txt");
     }
 
     public boolean accepts(EntryStruct struct) {
-        //TODO: No sure about this, but for now only accept files with extensions
-        return !struct.name.endsWith(".class") && !struct.name.endsWith(".java") && !struct.name.endsWith("MANIFEST.MF") && struct.name.contains(".");
+
+        return !struct.name.endsWith(".class")
+                && !struct.name.endsWith(".java")
+                && !struct.name.endsWith("MANIFEST.MF")
+                && (allow(struct.name) || (struct.name.startsWith("META-INF/") && struct.name.contains(".")));
+    }
+
+    private Boolean allow(String name) {
+
+        for (String type : allowedFiles) {
+            if (name.endsWith(type)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public String replaceLine(String line) {
