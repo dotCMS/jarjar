39666a9e2b05e9aefbaf659bf30e39104f124e27
Jonathan Gamba <jonathan.gamba@dotcms.com>
2013-12-06 15:32:31 -0600

    Improving replacement on resources files.

--- src/main/com/tonicsystems/jarjar/resource/MatchableRule.java
+++ src/main/com/tonicsystems/jarjar/resource/MatchableRule.java
@@ -21,22 +21,36 @@ public class MatchableRule {
 
     public String replace(String input) {
 
+        Boolean strict = true;
+        if (this.pattern.contains(".**")) {
+            strict = false;
+        }
+
         //xxx.yyy.zzzz
-        String pattern = this.pattern.replaceAll("\\.", "\\\\.");
-        pattern = pattern.replaceAll("\\.\\*\\*", "\\.([\\\\w\\\\d_\\\\\\$]*)");
-        pattern = pattern.replaceAll("\\.\\*", "\\.([\\\\w\\\\d_\\\\\\$]*)");
         String replacement = this.replacement.replaceAll("@1", "\\$1");
+        String pattern = this.pattern.replaceAll("\\.", "\\\\.");
+        if (strict) {
+            pattern = pattern.replaceAll("\\\\.\\*", "(\\\\.((?![\\\\w\\\\d_\\\\\\$]*\\\\.))|(?![\\\\w\\\\.]))");
+            replacement = replacement.replaceAll("\\.\\$1", "\\$1");
+        } else {
+            pattern = pattern.replaceAll("\\.\\*\\*", "\\.([\\\\w\\\\d_\\\\\\$]*)");
+        }
 
         pattern = "(?<!\\.)" + pattern;
         String output = replaceAll(input, pattern, replacement);
 
         //xxx/yyy/zzzz
-        pattern = this.pattern.replaceAll("\\.", "/");
-        pattern = pattern.replaceAll("/\\*\\*", "/([\\\\w\\\\d_\\\\\\$]*)");
-        pattern = pattern.replaceAll("/\\*", "/([\\\\w\\\\d_\\\\\\$]*)");
         replacement = this.replacement.replaceAll("\\.", "/").replaceAll("@1", "\\$1");
+        pattern = this.pattern.replaceAll("\\.", "/");
+        if (strict) {
+            pattern = pattern.replaceAll("/\\*", "(/((?![\\\\w\\\\d_\\\\\\$]*/))|(?![\\\\w/]))");
+            replacement = replacement.replaceAll("/\\$1", "\\$1");
+            pattern = "(?<![\\./\\w])" + pattern;
+        } else {
+            pattern = pattern.replaceAll("/\\*\\*", "/([\\\\w\\\\d_\\\\\\$]*)");
+            pattern = "(?<=\\B/|(?<!/))" + pattern;
+        }
 
-        pattern = "(?<=\\B/|(?<!/))" + pattern;
         output = replaceAll(output, pattern, replacement);
 
         return output;
--- src/test/com/tonicsystems/jarjar/MatchableRuleTest.java
+++ src/test/com/tonicsystems/jarjar/MatchableRuleTest.java
@@ -39,9 +39,9 @@ public class MatchableRuleTest {
         matches("/org/test/spring/spring-dwr-3.0.xsd", "/jarjar/org/test/spring/spring-dwr-3.0.xsd");
         matches("ffff=org/test/spring/spring-dwr-3.0.xsd", "ffff=jarjar/org/test/spring/spring-dwr-3.0.xsd");
 
-        rule = new MatchableRule("org.*", "jarjar.org.@1");
+        rule = new MatchableRule("org.foo.*", "jarjar.org.foo.@1");
 
-        matches("org.xxx.yyy", "jarjar.org.xxx.yyy");
+        matches("org.xxx.yyy", "org.xxx.yyy");
         matches("org.foo", "jarjar.org.foo");
 
         matches("zzz org.foo baz", "zzz jarjar.org.foo baz");
@@ -55,20 +55,22 @@ public class MatchableRuleTest {
         matches("org.foo!", "jarjar.org.foo!");
 
         matches("org", "org");
-        matches("org.", "jarjar.org.");
+        matches("org.foo.", "jarjar.org.foo.");
         matches("xorg.foo", "xjarjar.org.foo");
 
-        matches("org.foo2,org.bar1", "jarjar.org.foo2,jarjar.org.bar1");
-        matches("org.foo$1,org.bar_x hey", "jarjar.org.foo$1,jarjar.org.bar_x hey");
+        //matches("org.foo2,org.bar1,org.foo1", "jarjar.org.foo2,org.bar1,jarjar.org.foo1");
+        matches("org.foo,org.bar1,org.foo", "jarjar.org.foo,org.bar1,jarjar.org.foo");
+        //matches("org.foo$1,org.foo_x hey", "jarjar.org.foo$1,jarjar.org.foo_x hey");
 
-        matches("META-INF/services/org.test", "META-INF/services/jarjar.org.test");
-        matches("org/test/spring/spring-dwr-3.0.xsd", "jarjar/org/test/spring/spring-dwr-3.0.xsd");
-        matches("/org/test/spring/spring-dwr-3.0.xsd", "/jarjar/org/test/spring/spring-dwr-3.0.xsd");
-        matches("ffff=org/test/spring/spring-dwr-3.0.xsd", "ffff=jarjar/org/test/spring/spring-dwr-3.0.xsd");
+        matches("META-INF/services/org.foo.test", "META-INF/services/jarjar.org.foo.test");
+        matches("org/foo/test/spring-dwr-3.0.xsd", "org/foo/test/spring-dwr-3.0.xsd");
+        matches("org/foo/spring-dwr-3.0.xsd", "jarjar/org/foo/spring-dwr-3.0.xsd");
+        matches("/org/foo/test/spring/spring-dwr-3.0.xsd", "/org/foo/test/spring/spring-dwr-3.0.xsd");
+        matches("ffff=org/foo/spring-dwr-3.0.xsd", "ffff=jarjar/org/foo/spring-dwr-3.0.xsd");
 
-        matches("<dd>ddddd</dd><zz>zzzz</zz><validator-class>org.xxx.yyy.JstlCoreTLV</validator-class><yy>yyyy</yy>",
-                "<dd>ddddd</dd><zz>zzzz</zz><validator-class>jarjar.org.xxx.yyy.JstlCoreTLV</validator-class><yy>yyyy</yy>");
-        String output = rule.replace("<dd>ddddd</dd><zz>zzzz</zz><validator-class>org.xxx.yyy.JstlCoreTLV</validator-class><yy>yyyy</yy>");
+        matches("<dd>ddddd</dd><zz>zzzz</zz><validator-class>org.foo.JstlCoreTLV</validator-class><yy>yyyy</yy>",
+                "<dd>ddddd</dd><zz>zzzz</zz><validator-class>jarjar.org.foo.JstlCoreTLV</validator-class><yy>yyyy</yy>");
+        String output = rule.replace("<dd>ddddd</dd><zz>zzzz</zz><validator-class>org.foo.JstlCoreTLV</validator-class><yy>yyyy</yy>");
 
         rule = new MatchableRule("org.directwebremoting.**", "com.dotcms.repackage.dwr_3rc2modified.org.directwebremoting.@1");
         matches("http://www.directwebremoting.org/schema/spring-dwr/spring-dwr-3.0.xsd=org/directwebremoting/spring/spring-dwr-3.0.xsd",
@@ -76,7 +78,7 @@ public class MatchableRuleTest {
         matches("http://www.directwebremoting.org/schema/spring-dwr/spring-dwr-3.0.xsd=org.directwebremoting.spring.spring-dwr-3.0.xsd",
                 "http://www.directwebremoting.org/schema/spring-dwr/spring-dwr-3.0.xsd=com.dotcms.repackage.dwr_3rc2modified.org.directwebremoting.spring.spring-dwr-3.0.xsd");
 
-        rule = new MatchableRule("org.directwebremoting.*", "com.dotcms.repackage.dwr_3rc2modified.org.directwebremoting.@1");
+        rule = new MatchableRule("org.directwebremoting.spring.*", "com.dotcms.repackage.dwr_3rc2modified.org.directwebremoting.spring.@1");
         matches("http://www.directwebremoting.org/schema/spring-dwr/spring-dwr-3.0.xsd=org/directwebremoting/spring/spring-dwr-3.0.xsd",
                 "http://www.directwebremoting.org/schema/spring-dwr/spring-dwr-3.0.xsd=com/dotcms/repackage/dwr_3rc2modified/org/directwebremoting/spring/spring-dwr-3.0.xsd");
         matches("http://www.directwebremoting.org/schema/spring-dwr/spring-dwr-3.0.xsd=org.directwebremoting.spring.spring-dwr-3.0.xsd",
