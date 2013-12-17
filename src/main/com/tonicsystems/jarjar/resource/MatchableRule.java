74b2d24f8a434d8e333639b8fe66c78b2f71c7b6
Jonathan Gamba <jonathan.gamba@dotcms.com>
2013-12-16 18:58:38 -0600

    Improved replacement on resources files.

--- src/main/com/tonicsystems/jarjar/resource/MatchableRule.java
+++ src/main/com/tonicsystems/jarjar/resource/MatchableRule.java
@@ -19,6 +19,12 @@ public class MatchableRule {
         this.replacement = replacement;
     }
 
+    /**
+     * Applies to a given input a defined rule (pattern and replacement)
+     *
+     * @param input
+     * @return
+     */
     public String replace(String input) {
 
         Boolean strict = true;
@@ -27,33 +33,92 @@ public class MatchableRule {
         }
 
         //xxx.yyy.zzzz
-        String replacement = this.replacement.replaceAll("@1", "\\$1");
-        String pattern = this.pattern.replaceAll("\\.", "\\\\.");
+        String pattern = PackagePattern(this.pattern, strict);
+        String replacement = PackageReplacement(this.replacement, strict);
+        String output = replaceAll(input, pattern, replacement);
+
+        //xxx/yyy/zzzz
+        pattern = PathPattern(this.pattern, strict);
+        replacement = PathReplacement(this.replacement, strict);
+        output = replaceAll(output, pattern, replacement);
+
+        return output;
+    }
+
+    /**
+     * Returns the regex for a given pattern, this patter will handle packages like formats xxx.yyy.zzz
+     *
+     * @param initialPattern
+     * @param strict
+     * @return
+     */
+    public static String PackagePattern(String initialPattern, Boolean strict) {
+
+        //xxx.yyy.zzzz
+        String pattern = initialPattern.replaceAll("\\.", "\\\\.");
         if (strict) {
             pattern = pattern.replaceAll("\\\\.\\*", "(\\\\.((?![\\\\w\\\\d_\\\\\\$]*\\\\.))|(?![\\\\w\\\\.]))");
-            replacement = replacement.replaceAll("\\.\\$1", "\\$1");
         } else {
             pattern = pattern.replaceAll("\\.\\*\\*", "\\.([\\\\w\\\\d_\\\\\\$]*)");
         }
 
-        pattern = "(?<![\\.-])" + pattern;
-        String output = replaceAll(input, pattern, replacement);
+        return "(?<![\\.-])" + pattern;
+    }
+
+    /**
+     * Returns the replacement after some clean up ready to be use for packages like formats xxx.yyy.zzz
+     *
+     * @param initialReplacement
+     * @param strict
+     * @return
+     */
+    public static String PackageReplacement(String initialReplacement, Boolean strict) {
+
+        String replacement = initialReplacement.replaceAll("@1", "\\$1");
+        if (strict) {
+            replacement = replacement.replaceAll("\\.\\$1", "\\$1");
+        }
+
+        return replacement;
+    }
+
+    /**
+     * Returns the regex for a given pattern, this patter will handle packages like paths xxx/yyy/zzz
+     *
+     * @param initialPattern
+     * @param strict
+     * @return
+     */
+    public static String PathPattern(String initialPattern, Boolean strict) {
 
         //xxx/yyy/zzzz
-        replacement = this.replacement.replaceAll("\\.", "/").replaceAll("@1", "\\$1");
-        pattern = this.pattern.replaceAll("\\.", "/");
+        String pattern = initialPattern.replaceAll("\\.", "/");
         if (strict) {
             pattern = pattern.replaceAll("/\\*", "(/((?![\\\\w\\\\d_\\\\\\$]*/))|(?![\\\\w/]))");
-            replacement = replacement.replaceAll("/\\$1", "\\$1");
             pattern = "(?<![\\./\\w-])" + pattern;
         } else {
             pattern = pattern.replaceAll("/\\*\\*", "/([\\\\w\\\\d_\\\\\\$]*)");
             pattern = "(?<=\\B/|(?<!/))" + pattern;
         }
 
-        output = replaceAll(output, pattern, replacement);
+        return pattern;
+    }
 
-        return output;
+    /**
+     * Returns the replacement after some clean up ready to be use for packages like paths xxx/yyy/zzz
+     *
+     * @param initialReplacement
+     * @param strict
+     * @return
+     */
+    public static String PathReplacement(String initialReplacement, Boolean strict) {
+
+        String replacement = initialReplacement.replaceAll("\\.", "/").replaceAll("@1", "\\$1");
+        if (strict) {
+            replacement = replacement.replaceAll("/\\$1", "\\$1");
+        }
+
+        return replacement;
     }
 
     private String replaceAll(String input, String pattern, String replacement) {
